3b8559964861808c7af77ebe8c5e3b21638896df
John Lindquist <johnlindquist@gmail.com>
2021-09-16 13:07:52 -0600

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const script: string = core.getInput('script')
+    core.info(`Run the ${script} script ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
+    // core.setOutput('time', new Date().toTimeString())
+  } catch (error: any) {
     core.setFailed(error.message)
   }
 }
