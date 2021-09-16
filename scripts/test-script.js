aa555179722eef1ff6b3eca1cfc3966dff033cee
John Lindquist <johnlindquist@gmail.com>
2021-09-16 14:09:42 -0600

    testing

---
+++ scripts/test-script.js
@@ -0,0 +1,2 @@
+/* eslint-disable no-console */
+console.log('Testing a script')
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-undef */
 import * as core from '@actions/core'
 import minimist from 'minimist'
+import '@johnlindquist/kit'
 
 async function run(): Promise<void> {
   try {
+    core.info(JSON.stringify(process.env))
     const {script} = minimist(process.argv.slice(2))
 
     core.info(`Run the ${script} script ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
@@ -11,6 +14,7 @@ async function run(): Promise<void> {
     // core.debug(new Date().toTimeString())
 
     core.setOutput('script', script)
+    await kit(script)
   } catch (error) {
     core.setFailed((error as Error).message)
   }
