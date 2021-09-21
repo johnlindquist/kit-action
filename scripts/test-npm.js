1aababd791dae6cfe0fdcec237a4128f05e54f97
John Lindquist <johnlindquist@gmail.com>
2021-09-20 23:20:25 -0600

    testing

--- scripts/test-npm.js
+++ scripts/test-npm.js
@@ -1,9 +1,7 @@
 console.log(`👀 BEFORE NPM`)
 
 let { context } = await npm("@actions/github")
-let { Octokit } = await npm("@octokit/rest")
 
 console.log({ context })
-console.log({ Octokit })
 
 console.log(`🟢 AFTER NPM`)
