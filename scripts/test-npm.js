fc580e4536caa86a6f5dff3f53b7d2936d16c88d
John Lindquist <johnlindquist@gmail.com>
2021-09-20 23:28:38 -0600

    testing

--- scripts/test-npm.js
+++ scripts/test-npm.js
@@ -3,7 +3,8 @@ console.log(`☝️ ${kenvPath()}`)
 await $`which npm`
 
 let { context } = await npm("@actions/github")
-
 console.log({ context })
+let { Octokit } = await npm("@octokit/rest")
+console.log({ Octokit })
 
 console.log(`🟢 AFTER NPM`)
