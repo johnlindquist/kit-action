00108bc5b1337faf0a1d595676aa5c603609c25b
John Lindquist <johnlindquist@gmail.com>
2022-11-02 14:38:18 -0600

    fix(test): testing error

--- scripts/test-npm.js
+++ scripts/test-npm.js
@@ -1,10 +1,2 @@
-let { stdout } = await $`which npm`
-let { Octokit } = await npm("express")
-let json = await readJson("package.json")
-
-// if json includes @octkit/rest, then log "found"
-if (json.dependencies["express"]) {
-  console.log("found")
-} else {
-  console.log("not found")
-}
+let { titleCase } = await npm("title-case")
+console.log(await arg("Enter a string to title case:"))
