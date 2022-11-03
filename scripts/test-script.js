80a6877189b0267e80534aba9fffce8dc9447e47
John Lindquist <johnlindquist@gmail.com>
2022-11-03 10:04:51 -0600

    fix(test): args

--- scripts/test-script.js
+++ scripts/test-script.js
@@ -1,5 +1,7 @@
 console.log(`Hello, world! 🌎`)
-let core = await npm("@actions/core")
 let value = await arg("hello")
+
+// only required when you want to use the output of this script in another step
+let core = await npm("@actions/core")
 core.setOutput("result", value)
 console.log(`Goodbye, world! ${value} 🌍`)
