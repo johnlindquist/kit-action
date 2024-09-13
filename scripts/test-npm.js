e716c0fdf5778333af694ea1d25d4f5c72cec751
John Lindquist <johnlindquist@gmail.com>
2024-09-13 16:05:51 -0600

    fix(npm): fixing npm for kit-action

--- scripts/test-npm.js
+++ scripts/test-npm.js
@@ -1,5 +1,7 @@
+console.log({ args })
 let { titleCase } = await npm("title-case")
 let result = titleCase(await arg("Enter a string to title case:"))
+console.log({ args })
 
 console.log("typeof titleCase", typeof titleCase)
 console.log("result", { result })
